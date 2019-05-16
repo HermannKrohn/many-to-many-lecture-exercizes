8a0ce8ad11c0aca9bd202816ddfeec3b24ea3dc1
joshua-miles <joshua@christetulit.org>
2019-05-16 08:44:55 -0500

    -

---
+++ README.md
@@ -0,0 +1,7 @@
+# Many to Many
+### Self Assessments
+
+##### 1) Use `select` and `map` to implement an effective many to many relationship
+1. open `program.rb`
+2. Fill in all the strings with `<>`. You write use any values you like
+3. Finish the airlines method on line 7 using `.select` and `.map`
---
+++ program.rb
@@ -0,0 +1,54 @@
+class Traveler
+
+    def initialize(name)
+        @name = name
+    end
+
+    def airlines
+        # your code here
+    end
+
+end
+
+class Ticket 
+
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    def initialize(airline, traveler)
+        @airline = airline
+        @traveler = traveler
+        @@all << self
+    end
+
+    def airline
+        @airline
+    end
+    
+    def traveler
+        @traveler
+    end
+   
+end
+
+class Airline 
+
+    def initialize(name)
+        @name = name
+    end
+
+end
+
+me = Traveler.new('<Your Name>')
+
+united = Airline.new('United Airline')
+spirit = Airline.new('Spirit')
+southwest = Airline.new('Southwest Airlines')
+
+ticket_1 = Ticket.new(united_airline, me)
+ticket_2 = Ticket.new(southwest, me)
+
+puts "My Airlines: #{me.airlines}"
